7e0037d0289782c80d9352af31a25b39ef7401ed
hatefpalizgar <hatef.palizgar@gmail.com>
2019-05-12 19:09:38 +0430

    add input/output streams

--- src/Phase1/Client/ClientForm.java
+++ src/Phase1/Client/ClientForm.java
@@ -5,115 +5,120 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 
 
-public class ClientForm  {
-	
-	public ClientForm(){
-		initComponents();
-		
-	}
-	
-	// Send button clicked
-	private void btnSendActionPerformed(ActionEvent e) {
-		//TODO write to client text field +
-		
-	}
-	
-	private void initComponents() {
-		// JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
-		// Generated using JFormDesigner Evaluation license - Hatef
-		this.frame = new JFrame();
-		this.scrollPane1 = new JScrollPane();
-		this.txtLog = new JTextArea();
-		this.scrollPane2 = new JScrollPane();
-		this.txtInput = new JTextField();
-		this.btnSend = new JButton();
-		
-		this.frame.setTitle("Client");
-		this.frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		Container ClientFormContentPane = this.frame.getContentPane();
-		
-		
-		this.txtLog.setEditable(false);
-		this.scrollPane1.setViewportView(this.txtLog);
-		
-		this.btnSend.setText("Send");
-		this.btnSend.addActionListener(e -> btnSendActionPerformed(e));
-		
-		GroupLayout ClientFormContentPaneLayout = new GroupLayout(ClientFormContentPane);
-		ClientFormContentPane.setLayout(ClientFormContentPaneLayout);
-		ClientFormContentPaneLayout.setHorizontalGroup(
-				ClientFormContentPaneLayout.createParallelGroup()
-						.addGroup(ClientFormContentPaneLayout.createSequentialGroup()
-										  .addGroup(ClientFormContentPaneLayout.createParallelGroup()
-															.addGroup(ClientFormContentPaneLayout.createSequentialGroup()
-																			  .addGap(280, 280, 280)
-																			  .addComponent(this.scrollPane2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-															.addGroup(ClientFormContentPaneLayout.createSequentialGroup()
-																			  .addGap(45, 45, 45)
-																			  .addGroup(ClientFormContentPaneLayout.createParallelGroup()
-																								.addGroup(ClientFormContentPaneLayout.createSequentialGroup()
-																												  .addComponent(this.txtInput, GroupLayout.PREFERRED_SIZE, 428, GroupLayout.PREFERRED_SIZE)
-																												  .addGap(18, 18, 18)
-																												  .addComponent(this.btnSend, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-																								.addComponent(this.scrollPane1, GroupLayout.PREFERRED_SIZE, 587, GroupLayout.PREFERRED_SIZE))))
-										  .addContainerGap(81, Short.MAX_VALUE))
-													  );
-		ClientFormContentPaneLayout.setVerticalGroup(
-				ClientFormContentPaneLayout.createParallelGroup()
-						.addGroup(ClientFormContentPaneLayout.createSequentialGroup()
-										  .addGap(41, 41, 41)
-										  .addComponent(this.scrollPane1, GroupLayout.PREFERRED_SIZE, 395, GroupLayout.PREFERRED_SIZE)
-										  .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 19, Short.MAX_VALUE)
-										  .addGroup(ClientFormContentPaneLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-															.addComponent(this.txtInput, GroupLayout.PREFERRED_SIZE, 44, GroupLayout.PREFERRED_SIZE)
-															.addComponent(this.btnSend, GroupLayout.PREFERRED_SIZE, 51, GroupLayout.PREFERRED_SIZE))
-										  .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-										  .addComponent(this.scrollPane2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-										  .addGap(22, 22, 22))
-													);
-		this.frame.pack();
-		this.frame.setLocationRelativeTo(this.frame.getOwner());
-		// JFormDesigner - End of component initialization  //GEN-END:initComponents
-	}
-	
-	// Generated using JFormDesigner Evaluation license - Hatef
-	private JFrame      frame;
-	private JScrollPane scrollPane1;
-	private JTextArea   txtLog;
-	private JScrollPane scrollPane2;
-	private JTextField  txtInput;
-	private JButton     btnSend;
-
-	
-	public JFrame getFrame(){
-		return frame;
-	}
-	
-	public void setFrame(JFrame frame){
-		this.frame = frame;
-	}
-	
-	public JTextArea getTxtLog(){
-		return txtLog;
-	}
-	
-	public void setTxtLog(JTextArea txtLog){
-		this.txtLog = txtLog;
-	}
-	
-	public JTextField getTxtInput(){
-		return txtInput;
-	}
-	
-	public void setTxtInput(JTextField txtInput){
-		this.txtInput = txtInput;
-	}
-	
-	public JButton getBtnSend(){
-		return btnSend;
-	}
-	
-	public void setBtnSend(JButton btnSend){
-		this.btnSend = btnSend;
-	}
+public class ClientForm{
+    private SocketHandler socketHandler;
+    // Generated using JFormDesigner Evaluation license - Hatef
+    private JFrame        frame;
+    private JScrollPane   scrollPane1;
+    private JTextArea     txtLog;
+    private JScrollPane   scrollPane2;
+    private JTextField    txtInput;
+    private JButton       btnSend;
+    
+    //constructor
+    public ClientForm(){
+        initComponents();
+        socketHandler = new SocketHandler();
+        socketHandler.establishConnection();
+        
+    }//End of constructor
+    
+    // Send button clicked
+    private void btnSendActionPerformed(ActionEvent e){
+        //TODO write to client text field +
+        socketHandler.sendMessage(getTxtInput().getText());
+    }
+    
+    private void initComponents(){
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - Hatef
+        this.frame = new JFrame();
+        this.scrollPane1 = new JScrollPane();
+        this.txtLog = new JTextArea();
+        this.scrollPane2 = new JScrollPane();
+        this.txtInput = new JTextField();
+        this.txtInput.setEnabled(false);
+        this.btnSend = new JButton();
+        this.frame.setTitle("Client");
+        this.frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        Container ClientFormContentPane = this.frame.getContentPane();
+        this.txtLog.setEditable(false);
+        this.scrollPane1.setViewportView(this.txtLog);
+        this.btnSend.setText("Send");
+        this.btnSend.setEnabled(false);
+        this.btnSend.addActionListener(e -> btnSendActionPerformed(e));
+        GroupLayout ClientFormContentPaneLayout = new GroupLayout(ClientFormContentPane);
+        ClientFormContentPane.setLayout(ClientFormContentPaneLayout);
+        ClientFormContentPaneLayout.setHorizontalGroup(ClientFormContentPaneLayout.createParallelGroup().addGroup(
+                ClientFormContentPaneLayout.createSequentialGroup().addGroup(
+                        ClientFormContentPaneLayout.createParallelGroup().addGroup(
+                                ClientFormContentPaneLayout.createSequentialGroup().addGap(280 , 280 ,
+                                                                                           280).addComponent(
+                                        this.scrollPane2 , GroupLayout.PREFERRED_SIZE , GroupLayout.DEFAULT_SIZE ,
+                                        GroupLayout.PREFERRED_SIZE)).addGroup(
+                                ClientFormContentPaneLayout.createSequentialGroup().addGap(45 , 45 , 45).addGroup(
+                                        ClientFormContentPaneLayout.createParallelGroup().addGroup(
+                                                ClientFormContentPaneLayout.createSequentialGroup().addComponent(
+                                                        this.txtInput , GroupLayout.PREFERRED_SIZE , 428 ,
+                                                        GroupLayout.PREFERRED_SIZE).addGap(18 , 18 , 18).addComponent(
+                                                        this.btnSend , GroupLayout.DEFAULT_SIZE ,
+                                                        GroupLayout.DEFAULT_SIZE , Short.MAX_VALUE)).addComponent(
+                                                this.scrollPane1 , GroupLayout.PREFERRED_SIZE , 587 ,
+                                                GroupLayout.PREFERRED_SIZE)))).addContainerGap(81 , Short.MAX_VALUE)));
+        ClientFormContentPaneLayout.setVerticalGroup(ClientFormContentPaneLayout.createParallelGroup().addGroup(
+                ClientFormContentPaneLayout.createSequentialGroup().addGap(41 , 41 , 41).addComponent(this.scrollPane1 ,
+                                                                                                      GroupLayout.PREFERRED_SIZE ,
+                                                                                                      395 ,
+                                                                                                      GroupLayout.PREFERRED_SIZE).addPreferredGap(
+                        LayoutStyle.ComponentPlacement.RELATED , 19 , Short.MAX_VALUE).addGroup(
+                        ClientFormContentPaneLayout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(
+                                this.txtInput , GroupLayout.PREFERRED_SIZE , 44 ,
+                                GroupLayout.PREFERRED_SIZE).addComponent(this.btnSend , GroupLayout.PREFERRED_SIZE ,
+                                                                         51 ,
+                                                                         GroupLayout.PREFERRED_SIZE)).addPreferredGap(
+                        LayoutStyle.ComponentPlacement.RELATED).addComponent(this.scrollPane2 ,
+                                                                             GroupLayout.PREFERRED_SIZE ,
+                                                                             GroupLayout.DEFAULT_SIZE ,
+                                                                             GroupLayout.PREFERRED_SIZE).addGap(22 ,
+                                                                                                                22 ,
+                                                                                                                22)));
+        this.frame.pack();
+        this.frame.setLocationRelativeTo(this.frame.getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+    public void display(String string){
+        this.getTxtLog().append(string + "\n");
+    }
+    
+    public JFrame getFrame(){
+        return frame;
+    }
+    
+    public void setFrame(JFrame frame){
+        this.frame = frame;
+    }
+    
+    public JTextArea getTxtLog(){
+        return txtLog;
+    }
+    
+    public void setTxtLog(JTextArea txtLog){
+        this.txtLog = txtLog;
+    }
+    
+    public JTextField getTxtInput(){
+        return txtInput;
+    }
+    
+    public void setTxtInput(JTextField txtInput){
+        this.txtInput = txtInput;
+    }
+    
+    public JButton getBtnSend(){
+        return btnSend;
+    }
+    
+    public void setBtnSend(JButton btnSend){
+        this.btnSend = btnSend;
+    }
 }
--- src/Phase1/Client/SocketHandler.java
+++ src/Phase1/Client/SocketHandler.java
@@ -1,7 +1,6 @@
 package Phase1.Client;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 
 import static Phase1.Client.Main.clientForm;
@@ -18,29 +17,38 @@ class SocketHandler{
     }
     
     public void sendMessage(String string) throws Exception{
-        OutputStream os=server.getOutputStream();//for writing the data
-    
+        OutputStreamWriter writer = new OutputStreamWriter(os);
+        BufferedWriter bw = new BufferedWriter(writer);
+        bw.write(string);
+        clientForm.display("Me: " + string + "\n");
     }
     
     public void getMessage() throws Exception{
-        InputStream is=server.getInputStream();//for reading data
-    
-    }
-    
-    // establish connection to server
-    public void establishConnection() {
-        display("Connecting to server.....");
-        try{
-            server = new Socket("10.211.55.4" , 46857); //server ip: 10.211.55.4  port:46857
-            display("Connected to server: " + server.getLocalAddress() + " port:" + server.getLocalPort());
-        }catch(Exception e){
-            display("Error connecting to server");
-            e.printStackTrace();
+        InputStreamReader reader = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(reader);
+        String message;
+        while(clientForm.getFrame().isVisible()){
+            message = br.readLine();
+            clientForm.display(message);
         }
     }
     
-    private void display(String string){
-        clientForm.getTxtLog().append(string + "\n");
+    // establish connection to server
+    public boolean establishConnection(){
+        clientForm.display("Connecting to server.....");
+        try{
+            server = new Socket("10.211.55.4" , 46857); //server ip: 10.211.55.4  port:46857
+            clientForm.display("Connected to server: " + server.getLocalAddress() + " port:" + server.getLocalPort());
+            InputStream is = server.getInputStream();//for reading data
+            OutputStream os = server.getOutputStream();//for writing the data
+            clientForm.getTxtInput().setEnabled(true);
+            clientForm.getBtnSend().setEnabled(true);
+            return true;
+        } catch(Exception e){
+            clientForm.display("Error connecting to server");
+            e.printStackTrace();
+            return false;
+        }
     }
 }
 
